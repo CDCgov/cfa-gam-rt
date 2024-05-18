97afd86943a444d57d35ad39f535d230e1c8d32c
Zachary Susswein <utb2@cdc.gov>
2024-05-18 18:17:41 -0400

    Checkers for invalid inputs outside of type errors
    - Negative elements
    - Elements of vector don't sum to one

--- R/checkers.R
+++ R/checkers.R
@@ -1,8 +1,108 @@
+#' Check all elements are 0 or positive
+check_elements_non_neg <- function(x, arg = "x", call = rlang::caller_env()) {
+  is_pos <- x >= 0
+  if (!all(is_pos)) {
+    cli::cli_abort(
+      c("{.arg {arg}} has negative elements",
+        "!" = "All elements must be 0 or greater",
+        "i" = "Elements {.val {which(!is_pos)}} are negative"
+      ),
+      class = "RtGam_invalid_input",
+      call = call
+    )
+  }
+  invisible()
+}
+
+#' Check vector sums to one
+check_sums_to_one <- function(x,
+                              arg = "x",
+                              call = rlang::caller_env(),
+                              tol = 1e-8) {
+  diff <- abs(sum(x) - 1)
+  if (diff > tol) {
+    cli::cli_abort(
+      c("{.arg {arg}} does not sum to one",
+        "i" = "Uses a tolerance of {.val {tol}}",
+        "!" = "Difference of {.val {diff}} is greater than {.val {tol}}"
+      ),
+      class = "RtGam_invalid_input",
+      call = call
+    )
+  }
+  invisible()
+}
+
+#' Basic type checks for input validation
+#'
+#' @param x Object with type checking
+#' @param arg Name of the argument supplying the object
+#' @param call Calling environment to be passed to the type checker
+#' @name type_checker
+#'
+#' @return NULL, invisibly
+NULL
+
+#' @rdname type_checker
+check_date <- function(x, arg = "x", call = rlang::caller_env()) {
+  if ((!rlang::is_integerish(x)) || (class(x) != "Date")) {
+    throw_type_error(
+      object = x,
+      arg_name = arg,
+      expected_type = "Date",
+      call = call
+    )
+  }
+  invisible()
+}
+
+#' @rdname type_checker
+check_vector <- function(x, arg = "x", call = rlang::caller_env()) {
+  # Lists are bare vectors, but we want truly vanilla vectors
+  if (!rlang::is_bare_vector(x) || class(x) == "list") {
+    throw_type_error(
+      object = x,
+      arg_name = arg,
+      expected_type = "vector",
+      call = call
+    )
+  }
+  invisible()
+}
+
+#' @rdname type_checker
+check_integer <- function(x, arg = "x", call = rlang::caller_env()) {
+  if (!rlang::is_bare_integerish(x)) {
+    throw_type_error(
+      object = x,
+      arg_name = arg,
+      expected_type = "integer",
+      call = call
+    )
+  }
+  invisible()
+}
+
+check_character <- function(x, arg = "x", call = rlang::caller_env()) {
+  if (!rlang::is_bare_character(x)) {
+    throw_type_error(
+      object = x,
+      arg_name = arg,
+      expected_type = "character",
+      call = call
+    )
+  }
+  invisible()
+}
+
 #' Throw an informative type error on a user-provided input
 #'
-#' Follows the
+#' Follows the guidance from [rlang::abort()] on applying a call and a class
+#' in the error message. Used as a base in type-checkers to throw a properly
+#' formatted error.
+#'
 #' @param object Object with incorrect type
-#' @param arg_name Name of the corresponding argument to `.object`
+#' @param arg_name Name of the argument corresponding to `object`
 #' @param expected_type The type that the user should provide instead
 #' @param call The calling environment to be reflected in the error message
 #'
@@ -13,8 +113,8 @@ throw_type_error <- function(object,
                              arg_name,
                              expected_type,
                              call = rlang::caller_env()) {
-  abort(
-    message = c("{.arg {.arg_name} has type {.obj_type_friendly {object}}",
+  cli::cli_abort(
+    c("{.arg {arg_name}} is {.obj_type_friendly {object}}",
       "i" = "Must be of type {.emph {expected_type}}"
     ),
     call = call,
--- tests/testthat/test-checkers.R
+++ tests/testthat/test-checkers.R
@@ -1,3 +1,92 @@
+test_that("Negative element check works", {
+  non_neg <- c(0, 1, 2)
+  has_neg <- c(-1, 1, 2)
+  call <- NULL
+  arg <- "test"
+
+  expect_error(check_elements_non_neg(has_neg, arg, call),
+    class = "RtGam_invalid_input"
+  )
+  expect_null(check_elements_non_neg(non_neg, arg, call))
+})
+
+test_that("Sums to one check works", {
+  sums_to_one <- c(0.2, 0.4, 0.4)
+  sums_to_more <- c(0.2, 0.4, 0.405)
+  call <- NULL
+  arg <- "test"
+
+  expect_error(check_sums_to_one(sums_to_more, arg, call),
+    class = "RtGam_invalid_input"
+  )
+  expect_null(check_sums_to_one(sums_to_one, arg, call))
+})
+
+test_that("Type-checking dates works", {
+  dates <- as.Date(c("2023-01-01", "2023-01-02"))
+  not_dates <- c("abc", "123")
+  arg <- "test"
+  call <- NULL
+
+  expect_error(check_date(not_dates, arg, call),
+    class = "RtGam_type_error",
+    regexp = "Date"
+  )
+  expect_null(check_date(dates, arg, call))
+})
+
+test_that("Type-checking vectors works", {
+  bare_vector <- c(1, 2, 3)
+  non_bare_vector <- as.Date(bare_vector)
+  non_vector <- list()
+  arg <- "test"
+  call <- NULL
+
+  expect_error(check_vector(non_bare_vector, arg, call),
+    class = "RtGam_type_error",
+    regexp = "vector"
+  )
+  expect_error(check_vector(non_vector, arg, call),
+    class = "RtGam_type_error",
+    regexp = "vector"
+  )
+  expect_null(check_vector(bare_vector, arg, call))
+})
+
+test_that("Type-checking integers works", {
+  integers <- c(1L, 2L, 3L)
+  integerish <- c(-1, 0, 1)
+  not_integers <- c("a", "b", "c")
+  tricky_not_integers <- as.Date(integers)
+  arg <- "test"
+  call <- NULL
+
+  expect_error(check_integer(not_integers, arg, call),
+    class = "RtGam_type_error",
+    regexp = "integer"
+  )
+
+  expect_error(check_integer(tricky_not_integers, arg, call),
+    class = "RtGam_type_error",
+    regexp = "integer"
+  )
+  expect_null(check_integer(integers, arg, call))
+  expect_null(check_integer(integerish, arg, call))
+})
+
+test_that("Type-checking strings works", {
+  char <- c("a", "b", "c")
+  non_char <- c(1, 2, 3)
+  arg <- "test"
+  call <- NULL
+
+  expect_error(check_character(non_char, arg, call),
+    class = "RtGam_type_error",
+    regexp = "character"
+  )
+  expect_null(check_character(char, arg, call))
+})
+
 test_that("Type error throws successfully", {
   object <- "Test string"
   arg_name <- "Test argument"
