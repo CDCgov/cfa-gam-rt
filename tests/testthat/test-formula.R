df79dccb54c6b22ea82a8f9523f7cb14374e7038
Zachary Susswein <utb2@cdc.gov>
2024-07-06 03:23:15 +0000

    Test warnings throw for suboptimal params

--- tests/testthat/test-formula.R
+++ tests/testthat/test-formula.R
@@ -32,3 +32,20 @@ test_that("Smooth basis dim created successfully", {
   expect_equal(c("global_trend"), names(smooth_basis_dim))
   expect_equal(15, smooth_basis_dim[["global_trend"]])
 })
+
+
+test_that("`warn_for_suboptimal_params()` throws warning", {
+  dat <- data.frame(
+    cases = 1:10,
+    reference_date = seq.Date(
+      from = as.Date("2023-12-1"),
+      length.out = 10,
+      by = "day"
+    ),
+    timepoint = 1:10 / 10
+  )
+  k <- 1
+
+  expect_warning(warn_for_suboptimal_params(dat, k, m = 10))
+  expect_null(warn_for_suboptimal_params(dat, k, m = 1))
+})
