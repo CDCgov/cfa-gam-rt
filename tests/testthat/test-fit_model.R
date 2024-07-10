e286e895a2738793e141b23dad9e55fde85efc21
Zachary Susswein <utb2@cdc.gov>
2024-07-10 17:48:55 +0000

    Default args in S3 methods w/ user-supplied in ...
    It makes documentation slightly more straightforward

--- tests/testthat/test-fit_model.R
+++ tests/testthat/test-fit_model.R
@@ -3,7 +3,7 @@ test_that("fit_model.RtGam_gam fits a model", {
   class(data) <- c("RtGam_gam", class(data))
   formula <- y ~ 1 + s(x)
 
-  fit_gam <- fit_model(data, formula, list())
+  fit_gam <- fit_model(data, formula)
 
   expect_s3_class(fit_gam, "gam")
 })
@@ -13,7 +13,18 @@ test_that("fit_model.RtGam_bam fits a model", {
   class(data) <- c("RtGam_bam", class(data))
   formula <- y ~ 1 + s(x)
 
-  fit_gam <- fit_model(data, formula, list())
+  fit_gam <- fit_model(data, formula)
 
   expect_s3_class(fit_gam, "bam")
+})
+
+test_that("... passes modified arg to `fit_model()`", {
+  data <- data.frame(x = 1:20, y = rnbinom(20, mu = 1:20, size = 1))
+  class(data) <- c("RtGam_gam", class(data))
+  formula <- y ~ 1 + s(x)
+
+  fit_gam <- fit_model(data, formula, family = "poisson")
+
+  expect_s3_class(fit_gam, "gam")
+  expect_true(grepl("poisson", fit_gam$family$family))
 })
