1a630685e5fc49cd678875a4fd01ad1a0c39fdb4
Zachary Susswein <utb2@cdc.gov>
2024-07-10 17:48:55 +0000

    Default args in S3 methods w/ user-supplied in ...
    It makes documentation slightly more straightforward

--- R/fit_model.R
+++ R/fit_model.R
@@ -1,30 +1,35 @@
-fit_model <- function(data, formula, user_supplied_args) {
+fit_model <- function(data, formula, ...) {
   UseMethod("fit_model")
 }
 
 #' @export
-fit_model.RtGam_gam <- function(data, formula, user_supplied_args) {
-  default_args <- list(
-    formula = formula,
-    data = data,
+fit_model.RtGam_gam <- function(
+    data,
+    formula,
     family = "nb",
-    method = "REML"
-  )
-  args <- utils::modifyList(default_args, user_supplied_args)
+    method = "REML",
+    ...) {
+  # Override the defaults in formals with the user-supplied args in dots
+  formal_arg_names <- Filter(function(x) x != "...", names(formals()))
+  formals <- as.list(environment())[formal_arg_names]
+  dots <- rlang::list2(...)
+  args <- utils::modifyList(formals, dots)
 
   do.call(mgcv::gam, args)
 }
 
 #' @export
-fit_model.RtGam_bam <- function(data, formula, user_supplied_args) {
-  default_args <- list(
-    formula = formula,
-    data = data,
+fit_model.RtGam_bam <- function(
+    data,
+    formula,
     family = "nb",
     method = "fREML",
-    discrete = TRUE
-  )
-  args <- utils::modifyList(default_args, user_supplied_args)
-
+    discrete = TRUE,
+    ...) {
+  # Override the defaults in formals with the user-supplied args in dots
+  formal_arg_names <- Filter(function(x) x != "...", names(formals()))
+  formals <- as.list(environment())[formal_arg_names]
+  dots <- rlang::list2(...)
+  args <- utils::modifyList(formals, dots)
   do.call(mgcv::bam, args)
 }
