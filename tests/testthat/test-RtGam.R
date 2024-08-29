489681ebd4a54023d00165ad02cb2d775e8cca86
Zachary Susswein <utb2@cdc.gov>
2024-08-29 10:31:12 -0400

    Tests for print and diagnostics

--- tests/testthat/test-RtGam.R
+++ tests/testthat/test-RtGam.R
@@ -7,8 +7,20 @@ test_that("RtGam parses inputs successfully", {
   group <- NULL
 
   fit <- RtGam(cases, dates, group)
+  expected_slots <- c(
+    "model",
+    "data",
+    "min_date",
+    "max_data",
+    "k",
+    "m",
+    "backend",
+    "formula",
+    "diagnostics"
+  )
 
   expect_s3_class(fit, "RtGam")
+  expect_equal(names(fit), expected_slots)
 })
 
 test_that("Non-supported backends throw error", {
