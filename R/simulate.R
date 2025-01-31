fb37a212bfeb28d6cd94ac769e78b53cbe791e7b
Zachary Susswein <utb2@cdc.gov>
2025-01-31 11:00:17 -0500

    Closer

--- R/simulate.R
+++ R/simulate.R
@@ -1,27 +1,27 @@
 #' Simulate forward process model
 simulate_sir <- function(
-    S0 = 9990,
-    I0 = 10,
+    S0 = 9900,
+    I0 = 100,
     R0 = 0,
-    Rt = 0.5 * sin(seq_len(100) / 15 * pi) + 1,
+    Rt = 0.5 * sin(seq_len(100) / 30 * pi) + 1,
     gamma = 0.1,
-    delay_pmf = c(0.1, 0.1, 0.2, 0.4, 0.3),
-    k = 15,
-    seed = 12345) {
+    delay_pmf = c(rep(0, 19), 1),
+    k = 100,
+    seed = 12345,
+    date_0 = as.Date("2023-01-01"),
+    day_of_week = FALSE) {
   S <- integer(length(Rt))
   I <- integer(length(Rt))
   R <- integer(length(Rt))
   beta <- double(length(Rt))
+  incidence <- integer(length(Rt))
 
   S[1] <- S0
   I[1] <- I0
   R[1] <- R0
 
-  incidence <- integer(length(Rt))
-  gi_mean <- 1 # gamma rv, shape = 1, rate = 1
-
   for (t in 1:(length(beta) - 1)) {
-    beta[t] <- Rt[t] / S[t]
+    beta[t] <- (Rt[t] / S[t]) * gamma
     dSdt <- -max(as.integer(beta[t] * S[t] * I[t]), 0L)
     dRdt <- max(as.integer(I[t] * gamma), 0)
     dIdt <- -dSdt - dRdt
@@ -30,5 +30,21 @@ simulate_sir <- function(
     I[t + 1] <- I[t] + dIdt
     R[t + 1] <- R[t] + dRdt
   }
-  return(list(S = S, I = I, R = R))
+
+  true_cases <- convolve(I, rev(delay_pmf), type = "open")
+  obs_cases <- rnbinom(
+    n = length(Rt),
+    mu = true_cases,
+    size = k
+  )
+
+  return(list(
+    S = S,
+    I = I,
+    R = R,
+    true_cases = true_cases,
+    true_rt = Rt,
+    obs_cases = obs_cases,
+    reference_date = date_0 + 0:(length(Rt) - 1)
+  ))
 }
