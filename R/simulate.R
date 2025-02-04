f89c1bbe5f19e1d7ce750e5190c2e75e334cc2bb
Zachary Susswein <utb2@cdc.gov>
2025-02-04 14:18:49 +0000

    Clean up docs

--- R/simulate.R
+++ R/simulate.R
@@ -1,33 +1,42 @@
 #' Simulate an SIR model with specified time-varying reproduction number
 #'
-#' Uses the
+#' Simulates the spread of an infectious disease using the
+#' Susceptible-Infectious-Recovered (SIR) compartment model. The simulator takes a
+#' vector of specified \eqn{R_t} values and uses them to calculate the transmissibility
+#' of the disease at each time point. The model is then used to simulate the number of
+#' susceptible, infectious, and recovered individuals over time with disease transmission
+#' at each time point matching the pre-specified \eqn{R_t}. The function also
+#' simulates the number of true and observed incident cases, convolving simulated
+#' incidence through a specified delay distribution and simulating from a negative
+#' binomial distribution.
 #'
+#' @param Rt A vector of time-varying reproduction number values. Default is a
+#'  modified sine curve, oscillating between 0.50 and 1.50 with a period of 20 days.
 #' @param S0 Initial number of susceptible individuals
 #' @param I0 Initial number of infected individuals
 #' @param R0 Initial number of recovered individuals
-#' @param Rt A vector of time-varying reproduction number values. Default is a
-#'  modified sine curve, oscillating between 0.50 and 1.50 with a period of 20 days.
 #' @param gamma Recovery rate, the inverse of the average infectious period
 #' @param delay_pmf Probability mass function for delay distribution.
 #'   Default is a vector with mean 3.
 #' @param k Dispersion parameter for negative binomial distribution
 #' @param seed Random seed for reproducibility
-#' @param date_0 Start date for the simulation. Only used to add dates to outputs for use
+#' @param date0 Start date for the simulation. Only used to add dates to outputs for use
 #'   with `RtGam()` and friends.
-#' @param day_of_week Logical flag for day of the week effect (not used)
+#' @param day_of_week Values to use for the day of week effect. Defaults to NULL for
+#'    no effect.
 #' @return A list containing the time series of S, I, R, beta, true cases,
 #'   true Rt, observed cases, and reference dates
 #' @export
 simulate_sir <- function(
+    Rt = 0.5 * sin(seq_len(100) / 10 * pi) + 1,
     S0 = 99000,
     I0 = 1000,
     R0 = 0,
-    Rt = 0.5 * sin(seq_len(100) / 10 * pi) + 1,
     gamma = 0.5,
-    delay_pmf = c(0, 0, 0.1, 0.1, 0.3, 0.2, 0.1, 0.1, 0.1),
+    delay_pmf = c(0.1, 0.2, 0.3, 0.4),
     k = 10,
     seed = 12345,
-    date_0 = as.Date("2023-01-01"),
+    date0 = as.Date("2023-01-01"),
     day_of_week = FALSE) {
   S <- integer(length(Rt))
   I <- integer(length(Rt))
@@ -41,7 +50,7 @@ simulate_sir <- function(
   N <- S0 + I0 + R0
 
   for (t in 1:(length(beta) - 1)) {
-    # Catch case where S is zero causes beta to be NA
+    # Catch case where S is zero causing beta to be NA
     if (S[t] == 0) {
       beta[t] <- 0
     } else {
@@ -71,12 +80,17 @@ simulate_sir <- function(
     incidence[t + 1] <- -dSdt
   }
 
-  true_cases <- convolve(incidence, rev(delay_pmf), type = "open")
-  obs_cases <- rnbinom(
-    n = length(Rt),
-    mu = true_cases,
-    size = k
-  )
+  true_cases_full <- convolve(incidence, rev(delay_pmf), type = "open")
+  # TODO: Add day of week
+  # Drop initial values biased by missing entries in the convolution
+  true_cases <- true_cases_full[length(delay_pmf):length(true_cases_full)]
+  withr::with_seed(seed, {
+    obs_cases <- rnbinom(
+      n = length(Rt),
+      mu = true_cases,
+      size = k
+    )
+  })
 
   return(list(
     S = S,
@@ -87,6 +101,7 @@ simulate_sir <- function(
     true_cases = true_cases,
     true_rt = Rt,
     obs_cases = obs_cases,
-    reference_date = date_0 + 0:(length(Rt) - 1)
+    reference_date = date0 + length(delay_pmf):length(true_cases_full),
+    rt_date = date0 + 0:(length(Rt) - 1)
   ))
 }
